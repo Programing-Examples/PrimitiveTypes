d96e852dd1ac4b7446d4eb78029ff189ba8ec890
01Omar10 <omardiaz88@gmail.com>
2017-09-18 00:14:29 +0200

    Play with variables

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -18,5 +18,23 @@ public class Main {
         System.out.println("Size of char: " + (Character.BYTES) + " bytes.");
         System.out.println("Size of float: " + (Float.BYTES) + " bytes.");
         System.out.println("Size of double: " + (Double.BYTES) + " bytes.");
+
+        System.out.println("Maximo short: " + Short.MAX_VALUE);
+        System.out.println("Maximo short: " + Short.MIN_VALUE);
+        System.out.println("Maximo long: " + Long.MAX_VALUE);
+
+        charVar = 'a';
+
+        System.out.println("Nuestro caracter es: " + Integer.valueOf(charVar));
+        System.out.println("Nuestro caracter es: " + Integer.valueOf('b'));
+        System.out.println("Nuestro caracter es: " + Character.toChars(1));
+
+        intVar = 2;
+        floatVar = (float) 2.5;
+
+        System.out.println("2 / 3 = " + (2.0/3.0));
+
+
+
     }
 }
