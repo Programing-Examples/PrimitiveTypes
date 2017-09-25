c85496a6ee5f5f47cae107dced0bdb633be197be
01Omar10 <omardiaz88@gmail.com>
2017-09-25 17:42:25 +0200

    Clean code, add tests

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -3,38 +3,33 @@ package main.java.com.company;
 public class Main {
 
     public static void main(String[] args) {
+        /* Declaracion de variables:
+        /* Tipo_de_dato nombre_variable valor_inicial(opcional)
+         */
         byte byteVar;
-        char charVar;
+        char charVar = 'a';
         short shortVar;
-        int intVar;
+        int intVar = 3;
         float floatVar;
         long longVar;
-        double doubleVar;
+        double doubleVar = 4.1;
 
-        System.out.println("Size of byte: " + (Byte.BYTES) + " bytes.");
-        System.out.println("Size of short: " + (Short.BYTES) + " bytes.");
-        System.out.println("Size of int: " + (Integer.BYTES) + " bytes.");
-        System.out.println("Size of long: " + (Long.BYTES) + " bytes.");
-        System.out.println("Size of char: " + (Character.BYTES) + " bytes.");
-        System.out.println("Size of float: " + (Float.BYTES) + " bytes.");
-        System.out.println("Size of double: " + (Double.BYTES) + " bytes.");
+        System.out.println("El valor de charVar es: " + charVar);
+        System.out.println("El valor de intVar es: " + intVar);
+        System.out.println("El valor de doubleVar es: " + doubleVar);
 
-        System.out.println("Maximo short: " + Short.MAX_VALUE);
-        System.out.println("Maximo short: " + Short.MIN_VALUE);
+        System.out.println("Un byte es: " + (Byte.BYTES) + " bytes.");
+        System.out.println("Un short son: " + (Short.BYTES) + " bytes.");
+        System.out.println("Un int son: " + (Integer.BYTES) + " bytes.");
+        System.out.println("Un long son: " + (Long.BYTES) + " bytes.");
+        System.out.println("Un char son: " + (Character.BYTES) + " bytes.");
+        System.out.println("Un float son: " + (Float.BYTES) + " bytes.");
+        System.out.println("Un double son: " + (Double.BYTES) + " bytes.");
+
+        System.out.println("Maximo valor short: " + Short.MAX_VALUE);
+        System.out.println("Minimo valor short: " + Short.MIN_VALUE);
         System.out.println("Maximo long: " + Long.MAX_VALUE);
-
-        charVar = 'a';
-
-        System.out.println("Nuestro caracter es: " + Integer.valueOf(charVar));
-        System.out.println("Nuestro caracter es: " + Integer.valueOf('b'));
-        System.out.println("Nuestro caracter es: " + Character.toChars(1));
-
-        intVar = 2;
-        floatVar = (float) 2.5;
-
-        System.out.println("2 / 3 = " + (2.0/3.0));
-
-
+        System.out.println("Minimo long: " + Long.MIN_VALUE);
 
     }
 }
--- src/test/java/com/company/MainTest.java
+++ src/test/java/com/company/MainTest.java
@@ -2,7 +2,8 @@ package test.java.com.company;
 
 import main.java.com.company.Main;
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertTrue;
+
+import static org.testng.Assert.assertEquals;
 
 
 /**
@@ -12,9 +13,39 @@ public class MainTest extends Main {
 
 
     @Test
+    public void bytesSize() {
+        // This is the amount of bits used to store an int
+        assertEquals(1, Byte.BYTES);
+    }
+
+    @Test
     public void integerSize() {
         // This is the amount of bits used to store an int
-        assertTrue(0 == Integer.BYTES);
+        assertEquals(4, Integer.BYTES);
+    }
+
+    @Test
+    public void floatSize() {
+        // This is the amount of bits used to store an int
+        assertEquals(4, Float.BYTES);
+    }
+
+    @Test
+    public void doubleSize() {
+        // This is the amount of bits used to store an int
+        assertEquals(8, Double.BYTES);
+    }
+
+    @Test
+    public void longSize() {
+        // This is the amount of bits used to store an int
+        assertEquals(8, Long.BYTES);
+    }
+
+    @Test
+    public void characterSize() {
+        // This is the amount of bits used to store an int
+        assertEquals(2, Character.BYTES);
     }
 
 }
